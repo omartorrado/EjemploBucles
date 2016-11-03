88e3157594d2030f2e2fb27e4bf8d960cfb7877a
otorradomiguez <otorradomiguez@victoria2.danielcastelao.org>
2016-11-03 11:13:11 +0100

    usnado un bucle while y condicionales if else para que se deba meter un numero positivo o el programa se para al cabo de 3 intentos fallidos

--- src/ejemplobucles/Ejemplobucles.java
+++ src/ejemplobucles/Ejemplobucles.java
@@ -19,14 +19,37 @@ public class Ejemplobucles {
     public static void main(String[] args) {
         // TODO code application logic here
         Scanner sc=new Scanner(System.in);
-        System.out.println("Introduce un numero para ver su tabla de multiplicar");
+        System.out.println("Introduce un numero positivo para ver su tabla de multiplicar");
         int number=sc.nextInt();
-        BucleWhile bw=new BucleWhile();
-        bw.multiplicaWhile(number);
-        BucleFor bf=new BucleFor();
-        bf.MultiplicaFor(number);
-        BucleDoWhile bdw=new BucleDoWhile();
-        bdw.multiplicaDoWhile(number);
+        //usamos el valor absolutos del numero para que se convierta en positivo
+        //number=Math.abs(number);
+        //comprueba que el numero sea positivo o daveulve un error
+        int counter=1;
+        while (number<0){
+
+            if (counter<3){    
+                System.out.println("He dicho un numero positivo...");
+                number=sc.nextInt();
+                counter++;
+
+            }
+            else {
+                System.out.println("Al carallo");   
+                break;
+                    }
+        }
+        
+            if (number>=0){
+                BucleWhile bw=new BucleWhile();
+                bw.multiplicaWhile(number);
+                BucleFor bf=new BucleFor();
+                bf.MultiplicaFor(number);
+                BucleDoWhile bdw=new BucleDoWhile();
+                bdw.multiplicaDoWhile(number);
+            }
+        
+        
+  
     }
     
 }
